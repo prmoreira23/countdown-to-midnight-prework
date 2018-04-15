28a46885e0e347046ff4b8e071678ce5ea0f476f
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-15 03:13:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(sec)
+    until sec == 0 do
+        puts "#{sec} SECOND(S)!"
+        sec -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(sec)
+    until sec == 0 do
+        puts "#{sec} SECOND(S)!"
+        sleep(1)
+        sec -= 1
+    end
 end
